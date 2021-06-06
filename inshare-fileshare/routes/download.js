dfe08c4619b3bb1f4624ed201151554566416bd8
Debasis Pattanaik <67583843+debasis-pattanaik-au16@users.noreply.github.com>
2021-06-06 22:21:55 +0530

    Delete inshare-fileshare directory

--- inshare-fileshare/routes/download.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const downloadController = require('./../controllers/downloadController');
-
-router.get('/:uuid', downloadController.downloadFile);
-
-module.exports = router;
